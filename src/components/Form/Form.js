172ca80102c1f3014fd981e5c008c6efdfd81362
saransh <sethia.saransh.ss@gmail.com>
2024-06-08 09:32:40 +0530

    my second commit

---
+++ src/components/Form/Form.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Form.css';
+
+const Form = () => {
+  return (
+    <div>
+      <img src="https://www.ez.works/ez_works.webp" alt="ez"  />
+      <br />
+      <h3>Suite of Business Support Services</h3>
+    </div>
+  )
+}
+
+export default Form
---
+++ src/components/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Form from '../Form/Form'
+import Cards from '../Cards/Cards'
+
+const Home = () => {
+  return (
+    <div className='container'>
+      <Form />
+      <Cards />
+    </div>
+  )
+}
+
+export default Home
